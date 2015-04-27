c4e7f194b0c13e8186e914ef35b983800d1e3b22
Alan Cheung <alan.cheung@formax.hk>
2015-04-27 10:03:32 +0800

    first blood

---
+++ EchoClient.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.net.*;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+
+        if (args.length != 2) {
+            System.err.println(
+                    "Usage: java EchoClient <host name> <port number>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
+        try (
+                Socket echoSocket = new Socket(hostName, portNumber);
+                PrintWriter out =
+                new PrintWriter(echoSocket.getOutputStream(), true);
+                BufferedReader in =
+                new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+            ) {
+            int count = 1000*1000;
+            String input = "aaaaaaaaaa";
+            while (count > 0) {
+                out.println(input);
+                System.out.println("echo: " + in.readLine());
+                count --;
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " +
+                    hostName);
+            System.exit(1);
+        } 
+    }
+}
+
+
